4c7d6da112bfaa3f51b22340854f4bfe758f82aa
unknown <alec.thomas.quinn@gmail.com>
2019-06-28 03:47:38 -0500

    buttons toggle on click, now making buttons toggle on media query

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,14 @@
 const buttons = document.querySelectorAll('.cta');
 
+// making an array from the buttons Node List and applying a function to each one.
 Array.from(buttons).forEach(button => button.addEventListener('click', () => {
+    if (button.classList.contains('white')){
+        button.classList.remove('white');
+        button.classList.add('blue');
+    }
+    else if (button.classList.contains('blue')){
+        button.classList.remove('blue');
+        button.classList.add('white');
+    }
     console.log('clicked');
 }));
